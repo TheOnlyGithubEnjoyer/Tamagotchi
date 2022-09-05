15fbef3f426bd1b3169d092f83069fd555a503a2
Elliot <walker.elliot@elev.ga.ntig.se>
2022-09-05 13:29:14 +0200

    IDK

--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -4,4 +4,51 @@ using System.Collections.Generic;
 public class Tamagotchi
 {
 
+    private class hunger;
+
+    private class boredom;
+
+    private List<string> words = new List<string>() {"Hello"};
+
+    private bool isAlive;
+
+    private Random generator;
+
+    public string name;
+
+    public void Feed()
+        {
+
+        }
+
+    public void Hi() 
+    {
+
+    }
+
+    public void Teach()
+    {
+
+    }
+
+    public void Tick()
+    {
+
+    }       
+    
+    public void PrintStats()
+    {
+
+    }
+
+    public bool GetAlive()
+    {
+
+    }
+
+    private void ReduceBoredom()
+    {
+        
+    }
+
 }
