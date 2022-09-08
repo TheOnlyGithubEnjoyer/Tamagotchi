2d264867740dbb3069aa193f42823e21131bb066
Elliot <walker.elliot@elev.ga.ntig.se>
2022-09-08 10:44:51 +0200

    Latest

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -4,4 +4,36 @@ Console.WriteLine("Welcome to your Tamagotchi");
 
 Tamagotchi myTamagotchi = new Tamagotchi();
 
+Console.WriteLine("What would you like to name you Tamagotchi?");
+myTamagotchi.name = Console.ReadLine();
+
+Console.WriteLine($"Wow! {myTamagotchi.name} is a great name for a Tamagotchi!");
+Console.WriteLine($"Now, Press Enter to show the stats of {myTamagotchi.name}");
+
+while (myTamagotchi.GetAlive() == true){
+    Console.Clear();
+    myTamagotchi.PrintStats();
+    Console.WriteLine("DO SOMETHING!");
+    Console.WriteLine($"1. Tell {myTamagotchi.name} a joke");
+    Console.WriteLine($"2. Feed {myTamagotchi.name} some of you fucking sandwhich bitch");
+    Console.WriteLine($"3. Do nothing sucka");
+
+    string doSome = Console.ReadLine();
+    if (doSome == "1")
+    {
+        myTamagotchi.ReduceBoredom();
+    }
+    if (doSome == "2")
+    {
+        myTamagotchi.Feed();
+    }
+    if (doSome == "3")
+    {
+        myTamagotchi.Tick();
+    }
+}
+
+Console.WriteLine("Your Tamgotchi died");
+
+
 Console.ReadLine();
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 public class Tamagotchi
 {
 
-    private class hunger;
+    private int hunger = 0;
 
-    private class boredom;
+    private int boredom = 0;
 
     private List<string> words = new List<string>() {"Hello"};
 
@@ -16,9 +16,39 @@ public class Tamagotchi
 
     public string name;
 
+    public Tamagotchi (){
+        generator = new Random();
+        isAlive = true;
+    }
+
+    
     public void Feed()
         {
-
+            Console.WriteLine($"You fed {name} a biscuit and he is now less hungry.");
+            hunger -= 2;
+            if (hunger > 10)
+            {
+                isAlive = false;
+                Console.WriteLine($"You let {name} starve to death.");
+            }
+            if (hunger < -10){
+                isAlive = false;
+                Console.WriteLine($"You overfed {name}. He is now dead.");
+            }
+        }
+    public void ReduceBoredom()
+        {
+            Console.WriteLine($"{name} is now less bored of you!");
+            boredom -= 2;
+            if (boredom < -5)
+            {
+                Console.WriteLine($"{name} is having so much fun that he had a stroke");
+                isAlive = false;
+            }
+            if (boredom > 5)
+            {
+                Console.WriteLine($"{name} suddenly got depressed and offed himself -_-");
+            }
         }
 
     public void Hi() 
@@ -33,22 +63,24 @@ public class Tamagotchi
 
     public void Tick()
     {
-
+        hunger++;
+        boredom++;
+        if (hunger > 10 || boredom > 10)
+        {
+            isAlive = false;
+        }
     }       
     
     public void PrintStats()
     {
-
+        Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom}");
     }
 
     public bool GetAlive()
     {
-
+        return isAlive;
     }
 
-    private void ReduceBoredom()
-    {
-        
-    }
+
 
 }
